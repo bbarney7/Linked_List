6bfcd36abb3da85fda84b4af133e520f678d8b80
bbarney7 <brennen.barney@gmail.com>
2017-05-17 08:50:50 -0600

    Initial Commit

---
+++ McAfeeTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  McAfeeTest
+//
+//  Created by Brennen Barney on 5/17/17.
+//  Copyright © 2017 s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
